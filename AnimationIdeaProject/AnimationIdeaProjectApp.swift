c68044caae5504c3d6429cae5ce9a601e11dc9fa
Berk Bozkurt <BBozkurt@lcs.on.ca>
2022-04-28 11:34:01 -0400

    Initial Commit

---
+++ AnimationIdeaProject/AnimationIdeaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationIdeaProjectApp.swift
+//  AnimationIdeaProject
+//
+//  Created by Berk Bozkurt on 2022-04-28.
+//
+
+import SwiftUI
+
+@main
+struct AnimationIdeaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
